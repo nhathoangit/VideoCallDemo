98d34973f5934b8b9245b6b99443b2d3d3dec5d6
Nhat Hoang <nhat.hoang@apps-cyclone.com>
2019-07-12 11:36:47 +0700

    + Update code

---
+++ app/src/main/java/com/nhat910/videocalldemo/BaseActivity.java
@@ -0,0 +1,33 @@
+package com.nhat910.videocalldemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/*
+ * Created by NhatHoang on 12/07/2019.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+    private Unbinder mUnBinder;
+
+    public void setUnBinder(Unbinder unBinder) {
+        mUnBinder = unBinder;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setUnBinder(ButterKnife.bind(this));
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mUnBinder != null) {
+            mUnBinder.unbind();
+        }
+        super.onDestroy();
+    }
+}
